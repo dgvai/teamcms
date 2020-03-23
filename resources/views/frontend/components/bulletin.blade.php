d21e67873c013fe168e8f37f1ae50f6205f7e46a
Jalal Uddin <dgvai-git@users.noreply.github.com>
2020-03-23 22:59:27 +0600

    updated bulletins vi

--- resources/views/frontend/components/bulletin.blade.php
+++ resources/views/frontend/components/bulletin.blade.php
@@ -1,8 +1,16 @@
+<?php 
+use App\Models\Entities\SiteBulletines;
+$bullets = SiteBulletines::orderBy('created_at','desc')->get();
+?>
 <div class="container-fluid bullete">
     <div class="bullete-box text-center p-1">
         <span class="m-0 p-1">News Bulletin</span>
     </div>
     <div class="bullete-rail p-1">
-        <marquee class="news" direction="left">Lorem Ipsum Donor Amor One &bull; Lorem Ipsum Donor <a href="#">Amor</a> Two &bull; Lorem Third </marquee>
+        <marquee class="news" direction="left">
+            @foreach($bullets as $i => $bullet)
+            {{$bullet->news}} {!!($i+1 < count($bullets)) ? '&bull;' : ''!!}
+            @endforeach
+        </marquee>
     </div>
 </div>
