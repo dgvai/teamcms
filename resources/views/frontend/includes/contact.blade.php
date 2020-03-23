58699b7ca5bb56587c116e4a6d080f6b5dbb96be
Jalal Uddin <dgvai-git@users.noreply.github.com>
2020-03-23 23:01:16 +0600

    home vis

--- resources/views/frontend/includes/contact.blade.php
+++ resources/views/frontend/includes/contact.blade.php
@@ -2,44 +2,37 @@
 
 ?>
 
-<!-- Contact -->
 <div id="contact" class="section md-padding">
-    <!-- Container -->
     <div class="container">
-        <!-- Row -->
         <div class="row">
-            <!-- Section-header -->
             <div class="section-header text-center">
                 <h2 class="title">{{__('home.cntus')}}</h2>
             </div>
-            <!-- /Section-header -->
 
             <div class="row">
-                <!-- icons -->
                 <div class="col-md-4">
                     <div class="col-sm-12">
                         <div class="contact">
                             <i class="fa fa-phone"></i>
-                            <p>512-421-3940</p>
+                            <p>{{$site->contact_data->phone}}</p>
                         </div>
                     </div>
         
                     <div class="col-sm-12">
                         <div class="contact">
                             <i class="fa fa-envelope"></i>
-                            <p>email@support.com</p>
+                            <p>{{$site->contact_data->email}}</p>
                         </div>
                     </div>
         
                     <div class="col-sm-12">
                         <div class="contact">
                             <i class="fa fa-map-marker"></i>
-                            <p>1739 Bubby Drive</p>
+                            <p>{{$site->contact_data->address}}</p>
                         </div>
                     </div>
                 </div>
-                <!--/ icons -->
-                <!-- contact form -->
+
                 <div class="col-md-8">
                     <form class="base-form">
                         <input type="text" class="input" placeholder="Name">
@@ -49,13 +42,8 @@
                         <button class="main-btn">Send message</button>
                     </form>
                 </div>
-                <!-- /contact form -->
+
             </div>
         </div>
-        <!-- /Row -->
-
     </div>
-    <!-- /Container -->
-
 </div>
-<!-- /Contact -->
